d812442fa86e14d4d4165ffe38b78f47dfc498f9
Sebastián Nicaise <135925364+ZenichTUW@users.noreply.github.com>
2024-10-17 12:25:02 -0600

    Add files via upload

---
+++ practica2/src/routes.php
@@ -0,0 +1,50 @@
+<?php
+
+echo "hola mundo";
+
+
+//obtenemos en una variable el método de trabajo; get, put, delete, push
+$method = $_SERVER['REQUEST_METHOD'];
+//$_SERVER[INFORMACIÓN DEL SERVIDOR QUE DESEAMOS]: Variable reservada para obtener la información del array del servidor
+echo $method;
+
+// Remueve "/" del inicio
+$path = trim($_SERVER['PATH_INFO'], '/');
+//Path almacena la ruta o archivo desde donde hacemos la petición
+
+//importante anexar el /index.php/+Path utilizado
+//por ejemplo: http://localhost/practica2/public/index.php/prueba 
+
+echo $path;
+//Mostramos el final de la url
+
+// Divide la ruta por "/" para obtener el endpoint y el posible parámetro
+$segments = explode('/', $path);
+
+
+// Captura la cadena de consulta completa después del "?" (por ejemplo: "id=123&nombre=juan")
+$queryString = $_SERVER['QUERY_STRING'];
+
+// Parseamos la cadena de consulta a un arreglo asociativo
+parse_str($queryString, $queryParams);
+
+// Extraemos los parámetros de la consulta
+$cedula = isset($queryParams['cedula']) ? $queryParams['cedula'] : null;
+
+if ($path == "usuarios") {
+    switch ($method) {
+        case 'GET':
+            if ($cedula != "") {
+                echo json_encode(['Alert' => 'Hola: ' . $cedula]);
+            } else {
+                echo json_encode(['Alert' => 'a. Lista de todos los usuarios']);
+            }
+            break;
+        default:
+            Response::json(['error' => 'Método no permitido'], 405);
+    }
+} else {
+    include "error/response.html";
+}
+
+?>
